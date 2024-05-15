6bb21b08f242037168991ffe4e80e6c99a7026fb
Tyson Phalp <tyson.phalp@mx.com>
2024-05-15 16:07:06 -0600

    Fix cypress tests for auth

--- apps/authentication-service/cypress/e2e/express.cy.ts
+++ apps/authentication-service/cypress/e2e/express.cy.ts
@@ -1,6 +1,8 @@
+const PORT: number = (Cypress.env("PORT") as number) || 8089;
+
 describe("Express", () => {
   it("returns pong", () => {
-    cy.request("http://localhost:8081/api/ping").then(
+    cy.request(`http://localhost:${PORT}/api/ping`).then(
       (response: Cypress.Response<{ message: string }>) => {
         expect(response.status).to.eq(200);
         expect(response.body.message).to.eq("pong");
@@ -10,7 +12,7 @@ describe("Express", () => {
 
   it("handles not found", () => {
     cy.request({
-      url: "http://localhost:8081/api/pig",
+      url: `http://localhost:${PORT}/api/pig`,
       failOnStatusCode: false,
     })
       .its("status")
