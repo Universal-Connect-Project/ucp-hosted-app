4cbd135aeeaf653f800d77d62a7fba06868ac10c
Tyson Phalp <tyson.phalp@mx.com>
2024-05-28 09:26:35 -0600

    Some changes based on PR review

--- apps/authentication-service/src/test/handlers.ts
+++ apps/authentication-service/src/test/handlers.ts
@@ -1,14 +1,1 @@
-import { http, HttpRequestHandler, HttpResponse } from "msw";
-
-interface Ping404Response {
-  status: number;
-}
-
-export const handlers = [
-  http.get("http://localhost:8081/api/pig", () =>
-    HttpResponse.json<Ping404Response>({ status: 404 }),
-  ),
-  http.get("http://localhost:8081/api/error", () => {
-    throw new Error("error");
-  }),
-];
+export const handlers = [];
