5d18a4878a547dd8ec9f4c5763e7253cac36186b
Tyson Phalp <tyson.phalp@mx.com>
2024-06-12 09:04:20 -0600

    Added unit tests for Auth flow

---
+++ apps/authentication-service/cypress/e2e/clients.cy.ts
@@ -0,0 +1,10 @@
+// describe("Auth0", () => {
+//   it("returns valid token", () => {
+//     cy.request(`http://localhost:${PORT}/v1/clients/ping`).then(
+//       (response: Cypress.Response<{ message: string }>) => {
+//         expect(response.status).to.eq(200);
+//         expect(response.body.message).to.eq("pong");
+//       },
+//     );
+//   });
+// });
