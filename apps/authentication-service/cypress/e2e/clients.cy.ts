83393084de3447b1460eb07a55e3836a3b680974
Tyson Phalp <tyson.phalp@mx.com>
2024-06-17 16:53:23 -0600

    Remove unused

--- apps/authentication-service/cypress/e2e/clients.cy.ts
+++ apps/authentication-service/cypress/e2e/clients.cy.ts
@@ -1,3 +1,5 @@
+import { Client } from "auth0";
+
 describe("Client API", () => {
   const PORT: number = (Cypress.env("PORT") as number) || 8089;
   let ACCESS_TOKEN: string;
@@ -25,5 +27,37 @@ describe("Client API", () => {
       expect(response.status).to.eq(200);
       expect(response.body).property("client_id").to.eq(clientId);
     });
+  });
+
+  it("creates new client", () => {
+    // let newClientId: string;
+    const clientName = "__Test Client__";
+
+    cy.request({
+      method: "POST",
+      url: `http://localhost:${PORT}/v1/clients`,
+      headers: {
+        ContentType: "application/json",
+        Authorization: `Bearer ${ACCESS_TOKEN}`,
+      },
+      body: {
+        name: clientName,
+        description: "--DELETE ME--",
+      },
+    }).then((response: Cypress.Response<{ body: Client }>) => {
+      // newClientId = (response.body as unknown as Client).client_id;
+      expect(response.status).to.eq(200);
+      expect(response.body).property("name").to.eq(clientName);
+    });
+    // .then(() => {
+    //   cy.wait(5000);
+    //   cy.request({
+    //     method: "DELETE",
+    //     url: `https://${Cypress.env("AUTH0_DOMAIN")}/v1/clients/${newClientId}`,
+    //     headers: {
+    //       Authorization: `Bearer ${ACCESS_TOKEN}`,
+    //     },
+    //   });
+    // });
   });
 });
