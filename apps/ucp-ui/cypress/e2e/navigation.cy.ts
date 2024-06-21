07b3a9fd50b49f34661d6cc8b9612b704fee6b33
Wes Risenmay <wes.risenmay@mx.com>
2024-06-21 09:30:15 -0600

    e2e tests are now working with authentication

--- apps/ucp-ui/cypress/e2e/navigation.cy.ts
+++ apps/ucp-ui/cypress/e2e/navigation.cy.ts
@@ -1,15 +1,21 @@
 describe("Health", () => {
   it("renders something on the page", () => {
-    cy.visit("http://localhost:8080");
+    cy.visit("/");
 
     cy.findByText("Hello world!").should("exist");
   });
 
   it("renders a not found page", () => {
-    cy.visit("http://localhost:8080/ninjas");
+    cy.visit("/ninjas");
 
     cy.findByText("Not found").should("exist");
   });
 
-  it("logs you out", () => {});
+  it("logs you out", () => {
+    cy.visit("/");
+
+    cy.findByText("Log out").click();
+
+    cy.url().should("not.equal", Cypress.config("baseUrl"));
+  });
 });
