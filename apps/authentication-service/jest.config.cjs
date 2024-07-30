8158535c5ca42714a8c5433cb028a95e5cb9bada
Tyson Phalp <tyson.phalp@mx.com>
2024-07-29 23:30:59 -0600

    Ignore test files

--- apps/authentication-service/jest.config.cjs
+++ apps/authentication-service/jest.config.cjs
@@ -1,6 +1,10 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: "ts-jest",
+  coveragePathIgnorePatterns: [
+    "<rootDir>/src/test",
+    "<rootDir>/src/middleware",
+  ],
   setupFilesAfterEnv: ["<rootDir>/jestSetup.js"],
   moduleNameMapper: {
     "@/(.*)": "<rootDir>/src/$1",
