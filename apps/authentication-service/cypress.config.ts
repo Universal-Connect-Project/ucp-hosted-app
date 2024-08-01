868ac69346935e8510390cd8a6097a3214510950
Tyson Phalp <tyson.phalp@mx.com>
2024-08-01 11:46:44 -0600

    Move e2e env vars to a separate file

--- apps/authentication-service/cypress.config.ts
+++ apps/authentication-service/cypress.config.ts
@@ -9,10 +9,6 @@ export default defineConfig({
     AUTH0_AUDIENCE: process.env.AUTH0_AUDIENCE,
     AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
     AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
-    E2E_CLIENT_ID: process.env.E2E_CLIENT_ID,
-    E2E_CLIENT_SECRET: process.env.E2E_CLIENT_SECRET,
-    E2E_USERNAME: process.env.E2E_USERNAME,
-    E2E_PASSWORD: process.env.E2E_PASSWORD,
   },
   e2e: {
     setupNodeEvents(on, config) {
