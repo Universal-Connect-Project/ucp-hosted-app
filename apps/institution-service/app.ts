c310ca9510dcec0473e50a14bfaecb8086987dd6
Tyson Phalp <tyson.phalp@mx.com>
2024-08-06 09:58:00 -0600

    Add cypress to institution service (#27)

--- apps/institution-service/app.ts
+++ apps/institution-service/app.ts
@@ -1,11 +1,10 @@
-import dotenv from "dotenv";
 import express, { Request, Response } from "express";
 import logger from "morgan";
-import institutionRoutes from "./routes/institutionRoutes";
 
-dotenv.config();
+import institutionRoutes from "./routes/institutionRoutes";
+import { PORT } from "./shared/const";
+
 const app = express();
-const PORT = process.env.PORT || 3030;
 
 app.use(express.json()); // http://expressjs.com/en/api.html#express.json
 app.use(express.urlencoded({ extended: false })); // http://expressjs.com/en/5x/api.html#express.urlencoded
