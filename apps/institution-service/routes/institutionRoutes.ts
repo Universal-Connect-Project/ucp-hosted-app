20f9729541ab3dfb584824f91aefc745c49e1eca
Craig Tingey <craig.tingey@mx.com>
2024-08-07 13:55:26 -0600

    fix pipeline?

--- apps/institution-service/routes/institutionRoutes.ts
+++ apps/institution-service/routes/institutionRoutes.ts
@@ -1,10 +1,12 @@
-import "dotenv/config";
+import dotenv from "dotenv";
 import { Router } from "express";
 import { auth } from "express-oauth2-jwt-bearer";
 import {
   getAllInstitutions,
   getInstitutionCachedList,
 } from "../controllers/institutionController";
+
+dotenv.config();
 
 const validateAccessToken = auth({
   audience: process.env.AUTH0_AUDIENCE,
