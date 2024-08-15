367f2c8cf41217cc1a25bca6d56fbe9981bd43e3
Tyson Phalp <tyson.phalp@mx.com>
2024-08-15 14:49:35 -0600

    No make sense

--- packages/shared-utils/src/enums.ts
+++ packages/shared-utils/src/enums.ts
@@ -3,18 +3,18 @@ export const enum DefaultPermissions {
   USERINFO = "userinfo",
 }
 
-// export const enum WidgetHostPermissions {
-//   READ_KEYS = "read:keys",
-//   CREATE_KEYS = "create:keys",
-//   DELETE_KEYS = "delete:keys",
-//   ROTATE_KEYS = "rotate:keys",
-// }
+export const enum WidgetHostPermissions {
+  READ_KEYS = "read:keys",
+  CREATE_KEYS = "create:keys",
+  DELETE_KEYS = "delete:keys",
+  ROTATE_KEYS = "rotate:keys",
+}
 
-export const WidgetHostPermissions = {
-  READ_KEYS: "read:keys",
-  CREATE_KEYS: "create:keys",
-  DELETE_KEYS: "delete:keys",
-  ROTATE_KEYS: "rotate:keys",
-} as const;
-
-export type WidgetHostPermissions = keyof typeof WidgetHostPermissions;
+// export const WidgetHostPermissions: Record<string, string> = {
+//   READ_KEYS: "read:keys",
+//   CREATE_KEYS: "create:keys",
+//   DELETE_KEYS: "delete:keys",
+//   ROTATE_KEYS: "rotate:keys",
+// } as const;
+//
+// export type WidgetHostPermissions = keyof typeof WidgetHostPermissions;
