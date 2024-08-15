bfdf8f5a30534de2ddd45102a8ccf40eb5e6300a
Tyson Phalp <tyson.phalp@mx.com>
2024-08-15 14:42:55 -0600

    Another try

--- packages/shared-utils/src/enums.ts
+++ packages/shared-utils/src/enums.ts
@@ -3,9 +3,18 @@ export const enum DefaultPermissions {
   USERINFO = "userinfo",
 }
 
-export const enum WidgetHostPermissions {
-  READ_KEYS = "read:keys",
-  CREATE_KEYS = "create:keys",
-  DELETE_KEYS = "delete:keys",
-  ROTATE_KEYS = "rotate:keys",
-}
+// export const enum WidgetHostPermissions {
+//   READ_KEYS = "read:keys",
+//   CREATE_KEYS = "create:keys",
+//   DELETE_KEYS = "delete:keys",
+//   ROTATE_KEYS = "rotate:keys",
+// }
+
+export const WidgetHostPermissions = {
+  READ_KEYS: "read:keys",
+  CREATE_KEYS: "create:keys",
+  DELETE_KEYS: "delete:keys",
+  ROTATE_KEYS: "rotate:keys",
+} as const;
+
+type WidgetHostPermissions = keyof typeof WidgetHostPermissions;
