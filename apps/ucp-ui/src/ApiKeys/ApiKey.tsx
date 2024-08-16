349e604d69014d8ba68ed3fd3a1b949e20d16ea9
Wes Risenmay <wes.risenmay@mx.com>
2024-08-16 08:35:25 -0600

    added buttons to copy and display api keys

--- apps/ucp-ui/src/ApiKeys/ApiKey.tsx
+++ apps/ucp-ui/src/ApiKeys/ApiKey.tsx
@@ -1,9 +1,22 @@
-import { Stack, TextField, Typography } from "@mui/material";
-import React from "react";
+import {
+  IconButton,
+  InputAdornment,
+  OutlinedInput,
+  Stack,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
 import {
   InputSkeletonIfLoading,
   TextSkeletonIfLoading,
 } from "../shared/components/SkeletonIfLoading";
+import {
+  ContentCopyOutlined,
+  VisibilityOffOutlined,
+  VisibilityOutlined,
+} from "@mui/icons-material";
+import { useAppDispatch } from "../shared/utils/redux";
+import { displaySnackbar } from "../shared/reducers/snackbar";
 
 const ApiKey = ({
   isLoading,
@@ -14,6 +27,16 @@ const ApiKey = ({
   label: string;
   value?: string;
 }) => {
+  const [showValue, setShowValue] = useState(false);
+
+  const dispatch = useAppDispatch();
+
+  const handleCopyToClipboard = () => {
+    void navigator.clipboard.writeText(value as string);
+
+    dispatch(displaySnackbar(`${label} has been copied to your clipboard.`));
+  };
+
   return (
     <Stack spacing={1}>
       <TextSkeletonIfLoading isLoading={isLoading}>
@@ -22,13 +45,21 @@ const ApiKey = ({
         </Typography>
       </TextSkeletonIfLoading>
       <InputSkeletonIfLoading isLoading={isLoading}>
-        <TextField
+        <OutlinedInput
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton onClick={() => setShowValue(!showValue)}>
+                {showValue ? <VisibilityOffOutlined /> : <VisibilityOutlined />}
+              </IconButton>
+              <IconButton onClick={handleCopyToClipboard}>
+                <ContentCopyOutlined />
+              </IconButton>
+            </InputAdornment>
+          }
+          id={label}
           fullWidth
-          InputProps={{
-            readOnly: true,
-            id: label,
-          }}
-          type="password"
+          readOnly
+          type={showValue ? "text" : "password"}
           value={value}
         />
       </InputSkeletonIfLoading>
