b549923a76588ec0abbdc291d0aedffa7fe292e2
Tyson Phalp <tyson.phalp@mx.com>
2024-08-20 16:39:02 -0600

    Testing Auth0 Rate-limit issue

--- apps/authentication-service/src/init.ts
+++ apps/authentication-service/src/init.ts
@@ -1,17 +1,17 @@
 import cors from "cors";
 import helmet from "helmet";
 import nocache from "nocache";
-import { rateLimit } from "express-rate-limit";
+// import { rateLimit } from "express-rate-limit";
 import express, { Application, NextFunction, Request, Response } from "express";
 
-import { ConsoleColors } from "@/shared/enums";
+// import { ConsoleColors } from "@/shared/enums";
 import { errorHandler } from "@/middleware/errorMiddleware";
 import { notFoundHandler } from "@/middleware/notFoundMiddleware";
 import { clientsRoutes } from "@/resources/clients/clientsRoutes";
 
-const shouldDoRateLimitTest = process.env.RATE_LIMIT_TEST === "true" || false;
-const rateLimitWindow = shouldDoRateLimitTest ? 0.05 : 2; // 3 seconds (test) / 2 minutes (production) minutes
-const rateLimitCount = shouldDoRateLimitTest ? 5 : 100; // 5 (test) / 100 (production) requests per minute
+// const shouldDoRateLimitTest = process.env.RATE_LIMIT_TEST === "true" || false;
+// const rateLimitWindow = shouldDoRateLimitTest ? 0.05 : 2; // 3 seconds (test) / 2 minutes (production) minutes
+// const rateLimitCount = shouldDoRateLimitTest ? 5 : 100; // 5 (test) / 100 (production) requests per minute
 
 const initErrorHandling = (app: Application): void => {
   app.use(errorHandler);
@@ -23,22 +23,22 @@ export const initExpress = (app: Application): void => {
   app.use(express.json());
   app.set("json spaces", 2);
 
-  if (shouldDoRateLimitTest) {
-    console.log(
-      `${ConsoleColors.FgRed}Rate limit set to ${rateLimitCount} requests in ${rateLimitWindow * 60} seconds for testing purposes${ConsoleColors.Reset}`,
-    );
-  }
+  // if (shouldDoRateLimitTest) {
+  //   console.log(
+  //     `${ConsoleColors.FgRed}Rate limit set to ${rateLimitCount} requests in ${rateLimitWindow * 60} seconds for testing purposes${ConsoleColors.Reset}`,
+  //   );
+  // }
 
-  const limiter = rateLimit({
-    windowMs: rateLimitWindow * 60 * 1000,
-    limit: rateLimitCount,
-    message: `Too many requests from this IP, please try again after ${rateLimitWindow} minutes`,
-    handler: (_req, res, _next, options) =>
-      res
-        .status(options.statusCode)
-        .json({ message: options.message as string }),
-  });
-  app.use(limiter);
+  // const limiter = rateLimit({
+  //   windowMs: rateLimitWindow * 60 * 1000,
+  //   limit: rateLimitCount,
+  //   message: `Too many requests from this IP, please try again after ${rateLimitWindow} minutes`,
+  //   handler: (_req, res, _next, options) =>
+  //     res
+  //       .status(options.statusCode)
+  //       .json({ message: options.message as string }),
+  // });
+  // app.use(limiter);
 
   app.use(
     helmet({
