754dd94f670e0ddb31aef3553f9668627ea8e31d
Tyson Phalp <tyson.phalp@mx.com>
2024-08-29 12:33:10 -0600

    Holy crap, it worked

--- packages/shared-utils/src/consts.ts
+++ packages/shared-utils/src/consts.ts
@@ -1,17 +1,17 @@
-export enum DefaultPermissions {
-  OPENID = "openid",
-  USERINFO = "userinfo",
-  EMAIL = "email",
-}
+export const DefaultPermissions = {
+  OPENID: "openid",
+  USERINFO: "userinfo",
+  EMAIL: "email",
+};
 
-export enum UiClientPermissions {
-  READ_KEYS = "read:keys",
-  CREATE_KEYS = "create:keys",
-  DELETE_KEYS = "delete:keys",
-  ROTATE_KEYS = "rotate:keys",
-}
+export const UiClientPermissions = {
+  READ_KEYS: "read:keys",
+  CREATE_KEYS: "create:keys",
+  DELETE_KEYS: "delete:keys",
+  ROTATE_KEYS: "rotate:keys",
+};
 
-export enum WidgetHostPermissions {
-  READ_WIDGET_ENDPOINTS = "read:widget-endpoints",
-  WRITE_WIDGET_ENDPOINTS = "write:widget-endpoints",
-}
+export const WidgetHostPermissions = {
+  READ_WIDGET_ENDPOINTS: "read:widget-endpoints",
+  WRITE_WIDGET_ENDPOINTS: "write:widget-endpoints",
+};
