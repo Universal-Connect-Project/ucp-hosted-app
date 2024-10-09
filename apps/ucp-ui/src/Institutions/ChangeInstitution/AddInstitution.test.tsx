492d7f00dad90d9c07c9c66697455cc696a89907
Wes Risenmay <wes.risenmay@mx.com>
2024-10-09 13:35:48 -0600

    moved changeInstitution tests

--- apps/ucp-ui/src/Institutions/ChangeInstitution/AddInstitution.test.tsx
+++ apps/ucp-ui/src/Institutions/ChangeInstitution/AddInstitution.test.tsx
@@ -4,14 +4,12 @@ import {
   render,
   screen,
   userEvent,
-  waitFor,
   waitForLoad,
 } from "../../shared/test/testUtils";
 import AddInstitution from "./AddInstitution";
 import {
   INSTITUTION_CHANGE_ERROR_TEXT,
   INSTITUTION_ADD_SUCCESS_TEXT,
-  INSTITUTION_DRAWER_CLOSE_BUTTON_TEXT,
   INSTITUTION_FORM_ADD_KEYWORD_BUTTON_TEXT,
   INSTITUTION_FORM_ADD_ROUTING_NUMBER_BUTTON_TEXT,
   INSTITUTION_FORM_KEYWORD_LABEL_TEXT,
@@ -21,15 +19,12 @@ import {
   INSTITUTION_FORM_SUBMIT_BUTTON_TEXT,
   INSTITUTION_FORM_URL_LABEL_TEXT,
   INSTITUTIONS_ADD_INSTITUTION_BUTTON_TEXT,
-  REMOVE_INPUT_TEST_ID,
 } from "./constants";
 import { createInstitutionResponse } from "../../shared/test/testData/institution";
-import { REQUIRED_ERROR_TEXT } from "../../shared/constants/validation";
 import { server } from "../../shared/test/testServer";
 import { http, HttpResponse } from "msw";
 import { INSTITUTION_SERVICE_CREATE_INSTITUTION_URL } from "./api";
 import { TRY_AGAIN_BUTTON_TEXT } from "../../shared/components/constants";
-import { INVALID_URL_TEXT } from "../../shared/utils/validation";
 import { INSTITUTION_SERVICE_PERMISSIONS_URL } from "../api";
 
 const renderAndSubmit = async () => {
@@ -112,7 +107,7 @@ describe("<AddInstitution />", () => {
     expectLocation(`/institutions/${createInstitutionResponse.id}`);
   });
 
-  it("shows an error on api failure and allows retry", async () => {
+  it("shows an error on api failure, allows retry, and shows a success snackbar", async () => {
     server.use(
       http.post(
         INSTITUTION_SERVICE_CREATE_INSTITUTION_URL,
@@ -138,158 +133,6 @@ describe("<AddInstitution />", () => {
 
     expect(
       await screen.findByText(INSTITUTION_ADD_SUCCESS_TEXT),
-    ).toBeInTheDocument();
-  });
-
-  it("resets the form when the drawer is closed and reopened", async () => {
-    render(<AddInstitution />);
-
-    await userEvent.click(
-      await screen.findByRole("button", {
-        name: INSTITUTIONS_ADD_INSTITUTION_BUTTON_TEXT,
-      }),
-    );
-
-    await userEvent.type(
-      await screen.findByLabelText(
-        new RegExp(INSTITUTION_FORM_NAME_LABEL_TEXT),
-      ),
-      "Test Name",
-    );
-
-    await userEvent.click(
-      screen.getByRole("button", {
-        name: INSTITUTION_DRAWER_CLOSE_BUTTON_TEXT,
-      }),
-    );
-
-    await userEvent.click(
-      screen.getByRole("button", {
-        name: INSTITUTIONS_ADD_INSTITUTION_BUTTON_TEXT,
-      }),
-    );
-
-    expect(
-      await screen.findByLabelText(
-        new RegExp(INSTITUTION_FORM_NAME_LABEL_TEXT),
-      ),
-    ).toHaveValue("");
-  });
-
-  it("removes keywords and routing numbers when the delete button is pressed", async () => {
-    render(<AddInstitution />);
-
-    await userEvent.click(
-      await screen.findByRole("button", {
-        name: INSTITUTIONS_ADD_INSTITUTION_BUTTON_TEXT,
-      }),
-    );
-
-    await userEvent.click(
-      screen.getByRole("button", {
-        name: INSTITUTION_FORM_ADD_ROUTING_NUMBER_BUTTON_TEXT,
-      }),
-    );
-
-    expect(
-      await screen.findByLabelText(INSTITUTION_FORM_ROUTING_NUMBER_LABEL_TEXT),
-    ).toBeInTheDocument();
-
-    await userEvent.click(screen.getByTestId(REMOVE_INPUT_TEST_ID));
-
-    await waitFor(() =>
-      expect(
-        screen.queryByLabelText(INSTITUTION_FORM_ROUTING_NUMBER_LABEL_TEXT),
-      ).not.toBeInTheDocument(),
-    );
-
-    await userEvent.click(
-      screen.getByRole("button", {
-        name: INSTITUTION_FORM_ADD_KEYWORD_BUTTON_TEXT,
-      }),
-    );
-
-    expect(
-      await screen.findByLabelText(INSTITUTION_FORM_KEYWORD_LABEL_TEXT),
-    ).toBeInTheDocument();
-
-    await userEvent.click(screen.getByTestId(REMOVE_INPUT_TEST_ID));
-
-    await waitFor(() =>
-      expect(
-        screen.queryByLabelText(INSTITUTION_FORM_KEYWORD_LABEL_TEXT),
-      ).not.toBeInTheDocument(),
-    );
-  });
-
-  it("shows required validation errors", async () => {
-    render(<AddInstitution />);
-
-    await userEvent.click(
-      await screen.findByRole("button", {
-        name: INSTITUTIONS_ADD_INSTITUTION_BUTTON_TEXT,
-      }),
-    );
-
-    await userEvent.click(
-      screen.getByRole("button", { name: INSTITUTION_FORM_SUBMIT_BUTTON_TEXT }),
-    );
-
-    expect(await screen.findAllByText(REQUIRED_ERROR_TEXT)).toHaveLength(3);
-  });
-
-  it("shows url validation errors", async () => {
-    render(<AddInstitution />);
-
-    await userEvent.click(
-      await screen.findByRole("button", {
-        name: INSTITUTIONS_ADD_INSTITUTION_BUTTON_TEXT,
-      }),
-    );
-
-    await userEvent.type(
-      screen.getByLabelText(new RegExp(INSTITUTION_FORM_URL_LABEL_TEXT)),
-      "junk",
-    );
-
-    await userEvent.type(
-      screen.getByLabelText(new RegExp(INSTITUTION_FORM_LOGO_URL_LABEL_TEXT)),
-      "junk",
-    );
-
-    await userEvent.click(
-      screen.getByRole("button", { name: INSTITUTION_FORM_SUBMIT_BUTTON_TEXT }),
-    );
-
-    expect(await screen.findAllByText(INVALID_URL_TEXT)).toHaveLength(2);
-  });
-
-  it("shows routing number validation errors", async () => {
-    render(<AddInstitution />);
-
-    await userEvent.click(
-      await screen.findByRole("button", {
-        name: INSTITUTIONS_ADD_INSTITUTION_BUTTON_TEXT,
-      }),
-    );
-
-    await userEvent.click(
-      await screen.findByRole("button", {
-        name: INSTITUTION_FORM_ADD_ROUTING_NUMBER_BUTTON_TEXT,
-      }),
-    );
-
-    await userEvent.type(
-      await screen.findByLabelText(INSTITUTION_FORM_ROUTING_NUMBER_LABEL_TEXT),
-      "123",
-    );
-
-    await userEvent.click(
-      screen.getByRole("button", { name: INSTITUTION_FORM_SUBMIT_BUTTON_TEXT }),
-    );
-
-    expect(
-      await screen.findByText("Must be a 9 digit number"),
     ).toBeInTheDocument();
   });
 });
