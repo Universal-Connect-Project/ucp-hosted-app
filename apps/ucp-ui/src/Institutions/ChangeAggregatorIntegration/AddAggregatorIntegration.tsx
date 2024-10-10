d6c57c7f8ccd10c7c137f7fd3858f2b8b531e09c
Wes Risenmay <wes.risenmay@mx.com>
2024-10-10 11:44:06 -0600

    added the scaffolding for the add aggregator integration drawer

--- apps/ucp-ui/src/Institutions/ChangeAggregatorIntegration/AddAggregatorIntegration.tsx
+++ apps/ucp-ui/src/Institutions/ChangeAggregatorIntegration/AddAggregatorIntegration.tsx
@@ -1,23 +1,133 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { InstitutionWithPermissions } from "../api";
-import { Button } from "@mui/material";
+import { Button, Drawer, Typography } from "@mui/material";
 import { Add } from "@mui/icons-material";
-import { INSTITUTION_ADD_AGGREGATOR_INTEGRATION_BUTTON_TEXT } from "./constants";
+import {
+  INSTITUTION_ADD_AGGREGATOR_INTEGRATION_BUTTON_TEXT,
+  INSTITUTION_ADD_AGGREGATOR_INTEGRATION_SUBMIT_BUTTON_TEXT,
+  INSTITUTION_AGGREGATOR_INTEGRATION_FORM_AGGREGATOR_INSTITUTION_ID_LABEL_TEXT,
+} from "./constants";
+import DrawerContainer from "../../shared/components/Drawer/DrawerContainer";
+import DrawerContent from "../../shared/components/Drawer/DrawerContent";
+import DrawerCloseButton from "../../shared/components/Drawer/DrawerCloseButton";
+import { INSTITUTION_DRAWER_CLOSE_BUTTON_TEXT } from "../ChangeInstitution/constants";
+import DrawerTitle from "../../shared/components/Drawer/DrawerTitle";
+import styles from "./addAggregatorIntegration.module.css";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import { REQUIRED_ERROR_TEXT } from "../../shared/constants/validation";
+import TextField from "../../shared/components/Forms/TextField";
+import DrawerStickyFooter from "../../shared/components/Drawer/DrawerStickyFooter";
+import { LoadingButton } from "@mui/lab";
+
+interface Inputs {
+  aggregatorInstitutionId: string;
+}
+
+const formId = "changeAggregatorIntegration";
 
 const AddAggregatorIntegration = ({
   institution,
 }: {
   institution?: InstitutionWithPermissions;
 }) => {
-  const { canCreateAggregatorIntegration } = institution || {};
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleCloseDrawer = () => {
+    setIsOpen(false);
+  };
+
+  const handleOpenDrawer = () => {
+    setIsOpen(true);
+  };
+
+  const defaultValues = useMemo(
+    () => ({
+      aggregatorInstitutionId: "",
+    }),
+    [],
+  );
+
+  const { control, handleSubmit, reset } = useForm<Inputs>({
+    defaultValues,
+    mode: "onTouched",
+  });
+
+  useEffect(() => {
+    reset(defaultValues);
+  }, [institution, isOpen, reset, defaultValues]);
+
+  const onSubmit: SubmitHandler<Inputs> = (values) => console.log(values);
+
+  const { canCreateAggregatorIntegration, logo, name } = institution || {};
 
   return (
     <>
       {canCreateAggregatorIntegration && (
-        <Button startIcon={<Add />}>
+        <Button onClick={handleOpenDrawer} startIcon={<Add />}>
           {INSTITUTION_ADD_AGGREGATOR_INTEGRATION_BUTTON_TEXT}
         </Button>
       )}
+      <Drawer anchor="right" onClose={handleCloseDrawer} open={isOpen}>
+        <form
+          className={styles.form}
+          id={formId}
+          //   eslint-disable-next-line @typescript-eslint/no-misused-promises
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <DrawerContainer
+            closeButton={
+              <DrawerCloseButton handleClose={handleCloseDrawer}>
+                {INSTITUTION_DRAWER_CLOSE_BUTTON_TEXT}
+              </DrawerCloseButton>
+            }
+            footer={
+              <DrawerStickyFooter>
+                <LoadingButton
+                  // loading={isChangeInstitutionLoading}
+                  form={formId}
+                  type="submit"
+                  variant="contained"
+                >
+                  {INSTITUTION_ADD_AGGREGATOR_INTEGRATION_SUBMIT_BUTTON_TEXT}
+                </LoadingButton>
+              </DrawerStickyFooter>
+            }
+          >
+            <DrawerContent>
+              <DrawerTitle>Add Aggregator</DrawerTitle>
+              <div className={styles.nameLogoContainer}>
+                <img className={styles.institutionLogo} src={logo} />
+                <div className={styles.nameContainer}>
+                  <Typography>Institution</Typography>
+                  <Typography>{name}</Typography>
+                </div>
+              </div>
+              <Controller
+                name="aggregatorInstitutionId"
+                control={control}
+                rules={{ required: REQUIRED_ERROR_TEXT }}
+                render={({
+                  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                  field: { ref, ...fieldProps },
+                  fieldState: { error },
+                }) => (
+                  <TextField
+                    error={!!error}
+                    fullWidth
+                    helperText={error?.message}
+                    InputLabelProps={{ required: true }}
+                    label={
+                      INSTITUTION_AGGREGATOR_INTEGRATION_FORM_AGGREGATOR_INSTITUTION_ID_LABEL_TEXT
+                    }
+                    variant="filled"
+                    {...fieldProps}
+                  />
+                )}
+              />
+            </DrawerContent>
+          </DrawerContainer>
+        </form>
+      </Drawer>
     </>
   );
 };
