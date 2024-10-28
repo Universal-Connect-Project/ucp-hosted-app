8164a439e4225038c1519541b8688c524795012d
Wes Risenmay <wes.risenmay@mx.com>
2024-10-28 07:49:57 -0600

    fix tests

---
+++ apps/ucp-ui/src/shared/api/testData/aggregators.ts
@@ -0,0 +1,44 @@
+export const aggregatorsResponse = {
+  aggregators: [
+    {
+      id: 2,
+      name: "finicity",
+      displayName: "Finicity",
+      logo: "https://universalconnectproject.org/images/ucp-logo-icon.svg",
+      createdAt: "2024-10-23T13:33:43.875Z",
+      updatedAt: "2024-10-23T13:33:43.875Z",
+    },
+    {
+      id: 98,
+      name: "mx",
+      displayName: "MX",
+      logo: "https://content.moneydesktop.com/storage/MD_Assets/Ipad%20Logos/100x100/INS-3aeb38da-26e4-3818-e0fa-673315ab7754_100x100.png",
+      createdAt: "2024-10-23T13:33:43.844Z",
+      updatedAt: "2024-10-23T13:33:43.844Z",
+    },
+    {
+      id: 1,
+      name: "sophtron",
+      displayName: "Sophtron",
+      logo: "https://sophtron.com/Images/logo.png",
+      createdAt: "2024-10-23T13:33:43.872Z",
+      updatedAt: "2024-10-23T13:33:43.872Z",
+    },
+    {
+      id: 100,
+      name: "testExampleA",
+      displayName: "Test Example A",
+      logo: "https://universalconnectproject.org/images/ucp-logo-icon.svg",
+      createdAt: "2024-10-23T13:33:43.877Z",
+      updatedAt: "2024-10-23T13:33:43.877Z",
+    },
+    {
+      id: 99,
+      name: "testExampleB",
+      displayName: "Test Example B",
+      logo: "https://universalconnectproject.org/images/ucp-logo-icon.svg",
+      createdAt: "2024-10-23T13:33:43.879Z",
+      updatedAt: "2024-10-23T13:33:43.879Z",
+    },
+  ],
+};
