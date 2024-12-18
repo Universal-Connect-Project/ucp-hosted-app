924b30700f2e0b9ce165b55a436db977f8354d84
Wes Risenmay <wes.risenmay@mx.com>
2024-12-18 08:20:33 -0700

    created a ConfirmationDrawer abstraction and implemented it with ConfirmArchiveInstitution

--- apps/ucp-ui/src/Institutions/ChangeInstitution/ConfirmArchiveInstitution.tsx
+++ apps/ucp-ui/src/Institutions/ChangeInstitution/ConfirmArchiveInstitution.tsx
@@ -1,24 +1,15 @@
-import React, { FormEvent } from "react";
-import DrawerTitle from "../../shared/components/Drawer/DrawerTitle";
-import { Button, Typography } from "@mui/material";
-import DrawerContainer from "../../shared/components/Drawer/DrawerContainer";
-import DrawerCloseButton from "../../shared/components/Drawer/DrawerCloseButton";
+import React from "react";
 import {
   INSTITUTION_ARCHIVE_INSTITUTION_ERROR_TEXT,
   INSTITUTION_ARCHIVE_INSTITUTION_SUBMIT_BUTTON_TEXT,
-  INSTITUTION_DRAWER_CLOSE_BUTTON_TEXT,
 } from "../ChangeInstitution/constants";
-import DrawerContent from "../../shared/components/Drawer/DrawerContent";
-import DrawerStickyFooter from "../../shared/components/Drawer/DrawerStickyFooter";
-import { LoadingButton } from "@mui/lab";
-import styles from "./changeInstitution.module.css";
-import { useAppDispatch } from "../../shared/utils/redux";
-import { displaySnackbar } from "../../shared/reducers/snackbar";
-import FormSubmissionError from "../../shared/components/FormSubmissionError";
 import { Institution } from "../api";
 import { useDeleteInstitutionMutation } from "./api";
 import { useNavigate } from "react-router-dom";
 import { INSTITUTIONS_ROUTE } from "../../shared/constants/routes";
+import ConfirmationDrawer, {
+  UseMutation,
+} from "../../shared/components/Drawer/ConfirmationDrawer";
 
 const formId = "confirmRemoveInstitution";
 
@@ -29,68 +20,26 @@ const ConfirmArchiveInstitution = ({
   institution?: Institution;
   handleCloseDrawer: () => void;
 }) => {
-  const [mutateDeleteInstitution, { isError, isLoading }] =
-    useDeleteInstitutionMutation();
-
   const navigate = useNavigate();
 
-  const dispatch = useAppDispatch();
+  const onSuccess = () => navigate(`${INSTITUTIONS_ROUTE}`, { replace: true });
+  const successMessage = `${institution?.name} has been archived`;
 
   const { id } = institution || {};
 
-  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    mutateDeleteInstitution({ institutionId: id as string })
-      .unwrap()
-      .then(() => {
-        dispatch(displaySnackbar(`${institution?.name} has been archived`));
-
-        navigate(`${INSTITUTIONS_ROUTE}`, { replace: true });
-      })
-      .catch(() => {});
-  };
-
   return (
-    <form className={styles.form} id={formId} onSubmit={onSubmit}>
-      <DrawerContainer
-        closeButton={
-          <DrawerCloseButton handleClose={handleCloseDrawer}>
-            {INSTITUTION_DRAWER_CLOSE_BUTTON_TEXT}
-          </DrawerCloseButton>
-        }
-        footer={
-          <DrawerStickyFooter>
-            <LoadingButton
-              color="error"
-              loading={isLoading}
-              form={formId}
-              type="submit"
-              variant="contained"
-            >
-              {INSTITUTION_ARCHIVE_INSTITUTION_SUBMIT_BUTTON_TEXT}
-            </LoadingButton>
-            <Button color="inherit" onClick={handleCloseDrawer}>
-              CANCEL
-            </Button>
-          </DrawerStickyFooter>
-        }
-      >
-        <DrawerContent>
-          {isError && (
-            <FormSubmissionError
-              description={INSTITUTION_ARCHIVE_INSTITUTION_ERROR_TEXT}
-              formId={formId}
-              title="Something went wrong"
-            />
-          )}
-          <DrawerTitle>
-            Are you sure you want to archive this institution?
-          </DrawerTitle>
-          <Typography variant="body1">Archiving cannot be undone. </Typography>
-        </DrawerContent>
-      </DrawerContainer>
-    </form>
+    <ConfirmationDrawer
+      errorText={INSTITUTION_ARCHIVE_INSTITUTION_ERROR_TEXT}
+      description="Archiving cannot be undone."
+      formId={formId}
+      handleCloseDrawer={handleCloseDrawer}
+      onSuccess={onSuccess}
+      submitButtonText={INSTITUTION_ARCHIVE_INSTITUTION_SUBMIT_BUTTON_TEXT}
+      submitParams={{ institutionId: id }}
+      successMessage={successMessage}
+      title="Are you sure you want to archive this institution?"
+      useMutation={useDeleteInstitutionMutation as unknown as UseMutation}
+    />
   );
 };
 
---
+++ apps/ucp-ui/src/shared/components/Drawer/ConfirmationDrawer.tsx
@@ -0,0 +1,108 @@
+import React, { FormEvent } from "react";
+import DrawerTitle from "./DrawerTitle";
+import { Button, Typography } from "@mui/material";
+import DrawerContainer from "./DrawerContainer";
+import DrawerCloseButton from "./DrawerCloseButton";
+import DrawerContent from "./DrawerContent";
+import DrawerStickyFooter from "./DrawerStickyFooter";
+import { LoadingButton } from "@mui/lab";
+import styles from "./confirmationDrawer.module.css";
+import { useAppDispatch } from "../../utils/redux";
+import { displaySnackbar } from "../../reducers/snackbar";
+import FormSubmissionError from "../../components/FormSubmissionError";
+import { CONFIRMATION_DRAWER_CLOSE_BUTTON_TEXT } from "./confirmationDrawerConstants";
+
+export type UseMutation = () => [
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (arg: any) => { unwrap: () => Promise<void> },
+  {
+    isLoading: boolean;
+    isError: boolean;
+  },
+];
+
+const ConfirmationDrawer = ({
+  description,
+  errorText,
+  formId,
+  handleCloseDrawer,
+  submitParams,
+  onSuccess,
+  useMutation,
+  submitButtonText,
+  successMessage,
+  title,
+}: {
+  description: string;
+  errorText: string;
+  formId: string;
+  handleCloseDrawer: () => void;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  submitParams: any;
+  onSuccess: () => void;
+  submitButtonText: string;
+  successMessage: string;
+  title: string;
+  useMutation: UseMutation;
+}) => {
+  const [mutate, { isError, isLoading }] = useMutation();
+
+  const dispatch = useAppDispatch();
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    mutate(submitParams)
+      .unwrap()
+      .then(() => {
+        dispatch(displaySnackbar(successMessage));
+
+        onSuccess();
+
+        handleCloseDrawer();
+      })
+      .catch(() => {});
+  };
+
+  return (
+    <form className={styles.form} id={formId} onSubmit={onSubmit}>
+      <DrawerContainer
+        closeButton={
+          <DrawerCloseButton handleClose={handleCloseDrawer}>
+            {CONFIRMATION_DRAWER_CLOSE_BUTTON_TEXT}
+          </DrawerCloseButton>
+        }
+        footer={
+          <DrawerStickyFooter>
+            <LoadingButton
+              color="error"
+              loading={isLoading}
+              form={formId}
+              type="submit"
+              variant="contained"
+            >
+              {submitButtonText}
+            </LoadingButton>
+            <Button color="inherit" onClick={handleCloseDrawer}>
+              {CONFIRMATION_DRAWER_CLOSE_BUTTON_TEXT}
+            </Button>
+          </DrawerStickyFooter>
+        }
+      >
+        <DrawerContent>
+          {isError && (
+            <FormSubmissionError
+              description={errorText}
+              formId={formId}
+              title="Something went wrong"
+            />
+          )}
+          <DrawerTitle>{title}</DrawerTitle>
+          <Typography variant="body1">{description}</Typography>
+        </DrawerContent>
+      </DrawerContainer>
+    </form>
+  );
+};
+
+export default ConfirmationDrawer;
