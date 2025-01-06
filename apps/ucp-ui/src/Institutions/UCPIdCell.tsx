07b6453a3066f991c84a874c1462b263a3b749ce
Wes Risenmay <wes.risenmay@mx.com>
2025-01-06 11:46:32 -0700

    the copy button is functioning, but it overlaps the content

--- apps/ucp-ui/src/Institutions/UCPIdCell.tsx
+++ apps/ucp-ui/src/Institutions/UCPIdCell.tsx
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { MouseEvent } from "react";
 import styles from "./UCPId.module.css";
 import { IconButton, TableCell } from "@mui/material";
 import { ContentCopy } from "@mui/icons-material";
 import { TextSkeletonIfLoading } from "../shared/components/Skeleton";
+import { useAppDispatch } from "../shared/utils/redux";
+import { displaySnackbar } from "../shared/reducers/snackbar";
 
 const UCPIdCell = ({ id, isLoading }: { id: string; isLoading: boolean }) => {
+  const dispatch = useAppDispatch();
+
+  const handleCopyId = async (event: MouseEvent) => {
+    event.stopPropagation();
+
+    await navigator.clipboard.writeText(id);
+    dispatch(displaySnackbar("UCP ID has been copied to your clipboard"));
+  };
+
   return (
-    <TableCell className={styles.container}>
+    <TableCell className={styles.cell}>
       <TextSkeletonIfLoading isLoading={isLoading}>
-        <div>{id}</div>
+        <div className={styles.container}>
+          {id}
+          <div className={styles.copyButton}>
+            <IconButton onClick={(event) => void handleCopyId(event)}>
+              <ContentCopy />
+            </IconButton>
+          </div>
+        </div>
       </TextSkeletonIfLoading>
-      <div className={styles.copyButton}>
-        <IconButton>
-          <ContentCopy />
-        </IconButton>
-      </div>
     </TableCell>
   );
 };
