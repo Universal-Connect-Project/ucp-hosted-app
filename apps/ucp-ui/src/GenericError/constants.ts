0cacb9007e2872c175298cfb12c59e70963f99d2
Tyson Phalp <tyson.phalp@mx.com>
2025-01-06 14:19:37 -0700

    Fix html entities

--- apps/ucp-ui/src/GenericError/constants.ts
+++ apps/ucp-ui/src/GenericError/constants.ts
@@ -1,4 +1,4 @@
 export const GENERIC_ERROR_TITLE_TEXT = "Oops, something went wrong.";
 export const GENERIC_ERROR_PARAGRAPH_TEXT =
-  "It looks like the page you&apos;re looking for doesn&apos;t exist or something&apos;s broken on our end.";
+  "It looks like the page you're looking for doesn't exist or something's broken on our end.";
 export const GENERIC_ERROR_BUTTON_TEXT = "GO HOME";
