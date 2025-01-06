3dbc748674ec43c1fc359dc34c910e68e59a7fee
Tyson Phalp <tyson.phalp@mx.com>
2025-01-06 11:47:38 -0700

    Better positioning

--- apps/ucp-ui/src/GenericError/GenericError.tsx
+++ apps/ucp-ui/src/GenericError/GenericError.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button, Typography } from "@mui/material";
 import { ChevronRight } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
 
 import PageContent from "../shared/components/PageContent";
 import HeroImage from "./HeroImage";
@@ -8,27 +9,28 @@ import PageTitle from "../shared/components/PageTitle";
 import styles from "./genericError.module.css";
 
 const GenericError = () => {
+  const navigate = useNavigate();
+
   return (
     <>
       <PageContent>
         <div className={styles.pageContent}>
           <div className={styles.contentGrid}>
             <PageTitle>Oops, something went wrong.</PageTitle>
-            <br />
-            <Typography variant="body1">
-              It looks like the page you&apos;re looking for doesn&apos;t exist
-              or something&apos;s broken on our end.
-            </Typography>
-            <br />
-            <br />
-            <Button
-              endIcon={<ChevronRight />}
-              variant="contained"
-              color="primary"
-              href="/"
-            >
-              Go Home
-            </Button>
+            <div className={styles.description}>
+              <Typography variant="body1">
+                It looks like the page you&apos;re looking for doesn&apos;t
+                exist or something&apos;s broken on our end.
+              </Typography>
+              <Button
+                endIcon={<ChevronRight />}
+                variant="contained"
+                color="primary"
+                onClick={() => navigate("/")}
+              >
+                Go Home
+              </Button>
+            </div>
           </div>
           <div className={styles.contentGrid}>
             <HeroImage />
