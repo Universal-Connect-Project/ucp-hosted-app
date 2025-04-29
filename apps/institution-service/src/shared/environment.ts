d1b25bc0db3460084f09795429de8df7a8f24ca6
Wes Risenmay <wes.risenmay@mx.com>
2025-04-29 08:50:41 -0600

    fix performance service url issue

--- apps/institution-service/src/shared/environment.ts
+++ apps/institution-service/src/shared/environment.ts
@@ -1,2 +1,3 @@
-export const PERFORMANCE_SERVICE_URL =
-  process.env.PERFORMANCE_SERVICE_URL || "http://localhost:8090";
+export const PERFORMANCE_SERVICE_URL = process.env.REVIEW_APP
+  ? `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`
+  : process.env.PERFORMANCE_SERVICE_URL || "http://localhost:8090";
