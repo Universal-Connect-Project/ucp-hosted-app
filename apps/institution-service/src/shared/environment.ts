bef2cc8e0550657467d72ffd4b3a510e7eced531
Wes Risenmay <wes.risenmay@mx.com>
2025-04-29 09:24:24 -0600

    fix review app url

--- apps/institution-service/src/shared/environment.ts
+++ apps/institution-service/src/shared/environment.ts
@@ -1,3 +1,3 @@
 export const PERFORMANCE_SERVICE_URL = process.env.REVIEW_APP
-  ? `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`
+  ? `https://${process.env.HEROKU_APP_NAME}.herokuapp.com/performance-service`
   : process.env.PERFORMANCE_SERVICE_URL || "http://localhost:8090";
