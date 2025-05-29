f7f8c9d902983c14fd608dffe253d37af9cb7d0e
Wes Risenmay <wes.risenmay@mx.com>
2025-05-29 08:59:51 -0600

    fix unit tests

--- apps/authentication-service/src/test/handlers.ts
+++ apps/authentication-service/src/test/handlers.ts
@@ -12,10 +12,12 @@ import {
 } from "../test/testData/users";
 
 const domain: string = envs.AUTH0_DOMAIN;
+const tokenDomain: string = envs.AUTH0_TOKEN_DOMAIN;
 const AUTH0_BASE_URL = `https://${domain}/`;
+const AUTH0_TOKEN_BASE_URL = `https://${tokenDomain}/`;
 
 export const AUTH0_USER_BY_ID = `${AUTH0_BASE_URL}api/v2/users/:id`;
-export const AUTH0_AUTH_TOKEN = `${AUTH0_BASE_URL}oauth/token`;
+export const AUTH0_AUTH_TOKEN = `${AUTH0_TOKEN_BASE_URL}oauth/token`;
 export const AUTH0_CLIENTS = `${AUTH0_BASE_URL}api/v2/clients`;
 export const AUTH0_CLIENTS_BY_ID = `${AUTH0_BASE_URL}api/v2/clients/:id`;
 export const AUTH0_CLIENT_GRANTS = `${AUTH0_BASE_URL}api/v2/client-grants`;
