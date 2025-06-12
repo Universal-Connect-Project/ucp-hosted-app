8f1a36aa302fe6079ad4d0aa6876d019cfd35cf2
Craig Tingey <craig.tingey@mx.com>
2025-06-12 10:30:54 -0600

    prevent prod env config on review apps

--- apps/performance-service/src/dotEnv.ts
+++ apps/performance-service/src/dotEnv.ts
@@ -1,7 +1,10 @@
 import dotenv from "dotenv";
 import path from "path";
 
-if (process.env.NODE_ENV === "production") {
+if (
+  process.env.NODE_ENV === "production" &&
+  process.env.REVIEW_APP !== "true"
+) {
   dotenv.config({
     path: path.join(__dirname, "../env/production.env"),
   });
