a46e14b8de50b03f231db56ffad3e84c179b170e
Craig Tingey <craig.tingey@mx.com>
2025-06-12 11:05:53 -0600

    revert back to old var

--- apps/performance-service/src/dotEnv.ts
+++ apps/performance-service/src/dotEnv.ts
@@ -1,10 +1,7 @@
 import dotenv from "dotenv";
 import path from "path";
 
-if (
-  process.env.NODE_ENV === "production" &&
-  process.env.REVIEW_APP !== "true"
-) {
+if (process.env.PRODUCTION === "true") {
   dotenv.config({
     path: path.join(__dirname, "../env/production.env"),
   });
