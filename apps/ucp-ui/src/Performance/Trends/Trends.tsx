6ea161279a9c05d09694f235ea7fa9c7a49d85e6
Wes Risenmay <wes.risenmay@mx.com>
2025-06-12 10:14:37 -0600

    adding the trends chart

---
+++ apps/ucp-ui/src/Performance/Trends/Trends.tsx
@@ -0,0 +1,112 @@
+import React from "react";
+import { Stack, Typography } from "@mui/material";
+import TimeFrameSelect, {
+  useTimeFrameSelect,
+} from "../../shared/components/Forms/TimeFrameSelect";
+import { useGetAggregatorSuccessGraphDataQuery } from "./api";
+import { LineChart } from "@mui/x-charts";
+import { formatMaxTwoDecimals } from "../../shared/utils/format";
+import { format } from "date-fns";
+import { TZDate } from "@date-fns/tz";
+
+const EDTTimeZone = "America/New_York";
+
+const formatTooltip = (date: Date) => format(date, "MM/dd @ H:mm");
+
+const Trends = () => {
+  const { handleTimeFrameChange, timeFrame } = useTimeFrameSelect();
+
+  const { data } = useGetAggregatorSuccessGraphDataQuery({ timeFrame });
+  const performanceData = data?.performance?.map(
+    ({ midpoint, start, stop, ...rest }) => ({
+      ...rest,
+      midpoint: new TZDate(midpoint, EDTTimeZone),
+      start: new TZDate(start, EDTTimeZone),
+      stop: new TZDate(stop, EDTTimeZone),
+    }),
+  );
+  const midpointToStartAndEndMap: Record<
+    string,
+    {
+      start: Date;
+      stop: Date;
+    }
+  > =
+    performanceData?.reduce(
+      (acc, { midpoint, start, stop }) => ({
+        ...acc,
+        [midpoint.getTime()]: {
+          start,
+          stop,
+        },
+      }),
+      {},
+    ) || {};
+
+  const xAxis = [
+    {
+      dataKey: "midpoint",
+      // data: uniqueDates.map((date) => new Date(date)),
+      valueFormatter: (value: Date, context: { location: string }) => {
+        if (context.location === "tick") {
+          return format(value, "MM/dd");
+        } else if (context.location === "tooltip") {
+          const { start, stop } = midpointToStartAndEndMap?.[
+            value.getTime()
+          ] as {
+            start: Date;
+            stop: Date;
+          };
+
+          return `${formatTooltip(start)} - ${formatTooltip(stop)}`;
+        }
+      },
+    },
+  ];
+
+  const yAxis = [
+    {
+      max: 1,
+      min: 0,
+      valueFormatter: (value: number) => value * 100,
+    },
+  ];
+
+  const series = data?.aggregatorIds?.map((aggregatorId) => ({
+    curve: "linear",
+    dataKey: aggregatorId,
+    label: aggregatorId,
+    labelMarkType: "square",
+    valueFormatter: (value: number) =>
+      (value ?? null) !== null ? `${formatMaxTwoDecimals(value * 100)}%` : null,
+  }));
+
+  return (
+    <Stack spacing={3}>
+      <Typography variant="h5">Trends</Typography>
+      <Stack direction="column" spacing={2}>
+        <TimeFrameSelect onChange={handleTimeFrameChange} value={timeFrame} />
+      </Stack>
+      {data && (
+        <LineChart
+          dataset={performanceData}
+          height={400}
+          series={series}
+          slotProps={{
+            legend: {
+              direction: "horizontal",
+              position: {
+                horizontal: "start",
+                vertical: "top",
+              },
+            },
+          }}
+          xAxis={xAxis}
+          yAxis={yAxis}
+        />
+      )}
+    </Stack>
+  );
+};
+
+export default Trends;
