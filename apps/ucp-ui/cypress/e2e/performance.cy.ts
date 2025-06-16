06bafc79d8d53c97fa5b4e5e584d399cd1620b17
Wes Risenmay <wes.risenmay@mx.com>
2025-06-16 14:43:56 -0600

    added an e2e test for the performance charts

--- apps/ucp-ui/cypress/e2e/performance.cy.ts
+++ apps/ucp-ui/cypress/e2e/performance.cy.ts
@@ -1,18 +1,34 @@
 import { navigateToPerformance } from "../shared/navigation";
 
 describe("performance", () => {
-  it("shows aggregators and performance data", () => {
+  it("shows the performance charts", () => {
+    cy.loginWithWidgetRole();
+    cy.visit("/");
+    navigateToPerformance();
+
+    cy.findAllByText("MX").should("have.length", 3);
+
+    cy.get(".MuiMarkElement-root").should("have.length.at.least", 4);
+  });
+
+  it("shows aggregators and performance data by job type", () => {
     cy.loginWithoutWidgetRole();
     cy.visit("/");
 
     navigateToPerformance();
 
-    cy.findByText("MX").should("exist");
+    cy.findAllByText("MX").should("have.length", 3);
 
-    cy.findAllByText(/^\d+(?:\.\d+)?%$/).should("have.length.at.least", 1);
-    cy.findAllByText(/^\d+(?:\.\d+)?s$/).should("have.length.at.least", 1);
+    const percentDataRegex = /^\d+(?:\.\d+)?%$/;
+    cy.findAllByText(percentDataRegex).should("have.length.at.least", 1);
 
-    cy.findAllByText(/^\d+(?:\.\d+)?%\s*\|\s*\d+(?:\.\d+)?s$/).should(
+    const durationDataRegex = /^\d+(?:\.\d+)?s$/;
+    cy.findAllByText(durationDataRegex).should("have.length.at.least", 1);
+
+    const percentAndDurationDataRegex =
+      /^\d+(?:\.\d+)?%\s*\|\s*\d+(?:\.\d+)?s$/;
+
+    cy.findAllByText(percentAndDurationDataRegex).should(
       "have.length.at.least",
       1,
     );
