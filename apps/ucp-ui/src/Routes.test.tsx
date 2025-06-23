e06daddef4e4e258125ce96364cb9e2e74c575f2
Wes Risenmay <wes.risenmay@mx.com>
2025-06-23 13:39:44 -0600

    default to performance page

--- apps/ucp-ui/src/Routes.test.tsx
+++ apps/ucp-ui/src/Routes.test.tsx
@@ -1,43 +1,26 @@
 import React from "react";
 import { render, screen, userEvent } from "./shared/test/testUtils";
 import Routes from "./Routes";
-import { API_KEYS_CARD_TITLE_TEXT } from "./ApiKeys/constants";
+import { WIDGET_MANAGEMENT_PAGE_TITLE_TEXT } from "./ApiKeys/constants";
 import { INSTITUTIONS_PAGE_TITLE } from "./Institutions/constants";
 import {
+  SIDE_NAV_INSTITUTIONS_LINK_TEXT,
   SIDE_NAV_PERFORMANCE_LINK_TEXT,
   SIDE_NAV_WIDGET_MANAGEMENT_LINK_TEXT,
 } from "./Layout/constants";
 import { PERFORMANCE_PAGE_TITLE } from "./Performance/constants";
-import { PERFORMANCE_ROUTE } from "./shared/constants/routes";
 
 jest.mock("@auth0/auth0-react");
 
 describe("<Routes />", () => {
-  it("renders Institutions by default", async () => {
-    render(<Routes />, { shouldRenderRouter: false });
-
-    expect(await screen.findAllByText(INSTITUTIONS_PAGE_TITLE)).toHaveLength(2);
-  });
-
-  it("renders Widget Management", async () => {
-    render(<Routes />, { shouldRenderRouter: false });
-
-    await userEvent.click(
-      await screen.findByRole("link", {
-        name: SIDE_NAV_WIDGET_MANAGEMENT_LINK_TEXT,
-      }),
-    );
-
-    expect(
-      await screen.findByText(API_KEYS_CARD_TITLE_TEXT),
-    ).toBeInTheDocument();
-  });
-
-  it("renders Performance", async () => {
+  it("renders Performance by default", async () => {
     render(<Routes />, {
-      initialRoute: PERFORMANCE_ROUTE,
       shouldRenderRouter: false,
     });
+
+    expect(
+      await screen.findByRole("heading", { name: PERFORMANCE_PAGE_TITLE }),
+    ).toBeInTheDocument();
 
     await userEvent.click(
       await screen.findByRole("link", {
@@ -47,6 +30,46 @@ describe("<Routes />", () => {
 
     expect(
       await screen.findByRole("heading", { name: PERFORMANCE_PAGE_TITLE }),
+    ).toBeInTheDocument();
+  });
+
+  it("renders Institutions", async () => {
+    render(<Routes />, { shouldRenderRouter: false });
+
+    expect(
+      screen.queryByRole("heading", { name: INSTITUTIONS_PAGE_TITLE }),
+    ).not.toBeInTheDocument();
+
+    await userEvent.click(
+      await screen.findByRole("link", {
+        name: SIDE_NAV_INSTITUTIONS_LINK_TEXT,
+      }),
+    );
+
+    expect(
+      await screen.findByRole("heading", { name: INSTITUTIONS_PAGE_TITLE }),
+    ).toBeInTheDocument();
+  });
+
+  it("renders Widget Management", async () => {
+    render(<Routes />, { shouldRenderRouter: false });
+
+    expect(
+      screen.queryByRole("heading", {
+        name: WIDGET_MANAGEMENT_PAGE_TITLE_TEXT,
+      }),
+    ).not.toBeInTheDocument();
+
+    await userEvent.click(
+      await screen.findByRole("link", {
+        name: SIDE_NAV_WIDGET_MANAGEMENT_LINK_TEXT,
+      }),
+    );
+
+    expect(
+      await screen.findByRole("heading", {
+        name: WIDGET_MANAGEMENT_PAGE_TITLE_TEXT,
+      }),
     ).toBeInTheDocument();
   });
 });
