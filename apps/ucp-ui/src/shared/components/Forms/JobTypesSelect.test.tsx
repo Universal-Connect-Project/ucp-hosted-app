f8b0a11655867807dbb96c046d76755051ac6784
Wes Risenmay <wes.risenmay@mx.com>
2025-06-23 08:09:33 -0600

    filled in some tests for JobTypesSelect

--- apps/ucp-ui/src/shared/components/Forms/JobTypesSelect.test.tsx
+++ apps/ucp-ui/src/shared/components/Forms/JobTypesSelect.test.tsx
@@ -1,5 +1,62 @@
-describe("<JobTypesSelect />", () => {
-  it("renders the job types with a + in between them for the label, and with a | in between for the value", () => {});
+import React from "react";
+import { render, screen, userEvent } from "../../test/testUtils";
+import JobTypesSelect, { useJobTypesSelect } from "./JobTypesSelect";
+import { JOB_TYPES_LABEL_TEXT } from "./constants";
+import { supportsJobTypeMap } from "../../constants/jobTypes";
 
-  it("defaults to no job types selected and allows selecting multiple job types", async () => {});
+const TestComponent = () => {
+  const { handleJobTypesChange, jobTypes } = useJobTypesSelect();
+
+  return (
+    <>
+      <div>{JSON.stringify(jobTypes)}</div>
+      <JobTypesSelect onChange={handleJobTypesChange} value={jobTypes} />
+    </>
+  );
+};
+
+describe("<JobTypesSelect />", () => {
+  it("renders the job types with a + in between them for the label, and with a | in between for the value", async () => {
+    render(<TestComponent />);
+
+    await userEvent.click(screen.getByLabelText(JOB_TYPES_LABEL_TEXT));
+
+    const optionWithMultipleJobTypes = await screen.findByRole("option", {
+      name: `${supportsJobTypeMap.accountNumber.displayName} + ${supportsJobTypeMap.accountOwner.displayName}`,
+    });
+
+    expect(optionWithMultipleJobTypes).toBeInTheDocument();
+
+    await userEvent.click(optionWithMultipleJobTypes);
+
+    expect(
+      await screen.findByText(JSON.stringify(["accountNumber|accountOwner"])),
+    ).toBeInTheDocument();
+  });
+
+  it("defaults to no job types selected and allows selecting multiple job types", async () => {
+    render(<TestComponent />);
+
+    expect(await screen.findByText(JSON.stringify([]))).toBeInTheDocument();
+
+    await userEvent.click(screen.getByLabelText(JOB_TYPES_LABEL_TEXT));
+
+    await userEvent.click(
+      await screen.findByRole("option", {
+        name: supportsJobTypeMap.accountNumber.displayName,
+      }),
+    );
+
+    await userEvent.click(
+      await screen.findByRole("option", {
+        name: supportsJobTypeMap.accountOwner.displayName,
+      }),
+    );
+
+    expect(
+      await screen.findByText(
+        JSON.stringify(["accountNumber", "accountOwner"]),
+      ),
+    ).toBeInTheDocument();
+  });
 });
