fe08607ad277c950a0f54797c84fe7ec73d6d80f
Ashley Wright <ashley.wright@mx.com>
2025-07-15 15:16:13 -0600

    Fixed test and added new cypress tests

--- apps/ucp-ui/cypress/e2e/widgetDemo.cy.ts
+++ apps/ucp-ui/cypress/e2e/widgetDemo.cy.ts
@@ -1,16 +1,80 @@
+import { WIDGET_DEMO_PAGE_TITLE } from "../../src/Demo/constants";
 import { SIDE_NAV_DEMO_LINK_TEXT } from "../../src/Layout/constants";
 
-describe("Demo Widget", () => {
-  it("navigates to the demo page", () => {
+describe("Widget Demo", () => {
+  it("should load the demo landing page and display initial state", () => {
     cy.loginWithWidgetDemoPermissions();
     cy.visit("/");
     cy.findByRole("link", { name: SIDE_NAV_DEMO_LINK_TEXT }).click();
+    cy.contains(WIDGET_DEMO_PAGE_TITLE).should("be.visible");
+    cy.get('button[role="tab"]').contains("Connect").should("be.visible");
+    cy.get('button[role="tab"]').contains("Connections").should("be.visible");
+    cy.get('input[name="accountNumber"]').should("be.checked");
+    cy.get('[data-testid="aggregator-select"]').should("contain", "MX");
+    cy.contains("button", "Launch").should("be.visible");
+  });
 
+  it("should show an error if no job types are selected", () => {
+    cy.loginWithWidgetDemoPermissions();
+    cy.visit("/");
+    cy.findByRole("link", { name: SIDE_NAV_DEMO_LINK_TEXT }).click();
+    cy.get('input[name="accountNumber"]').uncheck();
+    cy.contains("button", "Launch").click();
+    cy.contains("Please select at least one job type.").should("be.visible");
+  });
+
+  it("should launch the demo with correct parameters for MX", () => {
+    cy.loginWithWidgetDemoPermissions();
+    cy.visit("/");
+    cy.findByRole("link", { name: SIDE_NAV_DEMO_LINK_TEXT }).click();
+    cy.get('input[name="accountOwner"]').check();
+    cy.contains("button", "Launch").click();
+
+    cy.get('[data-testid="demo-component"]').should("be.visible");
     cy.get("iframe")
       .its("0.contentDocument")
       .its("body")
       .within(() => {
         cy.findByText("Select your institution").should("exist");
       });
+  });
+
+  it("should launch the demo with correct parameters for Sophtron", () => {
+    cy.loginWithWidgetDemoPermissions();
+    cy.visit("/");
+    cy.findByRole("link", { name: SIDE_NAV_DEMO_LINK_TEXT }).click();
+    cy.get('[data-testid="aggregator-select"]').click();
+    cy.get('li[data-value="Sophtron"]').click();
+    cy.get('input[name="transactions"]').check();
+    cy.get('input[name="transactionHistory"]').check();
+    cy.contains("button", "Launch").click();
+
+    cy.get('[data-testid="demo-component"]').should("be.visible");
+    cy.get("iframe")
+      .its("0.contentDocument")
+      .its("body")
+      .within(() => {
+        cy.findByText("Select your institution").should("exist");
+      });
+  });
+
+  it("should reset the form when the reset button is clicked", () => {
+    cy.loginWithWidgetDemoPermissions();
+    cy.visit("/");
+    cy.findByRole("link", { name: SIDE_NAV_DEMO_LINK_TEXT }).click();
+    cy.contains("button", "Launch").click();
+
+    cy.get('[data-testid="demo-component"]').should("be.visible");
+    cy.get('button:contains("Reset")').click();
+    cy.get('[data-testid="demo-component"]').should("not.exist");
+    cy.contains("h3", "Configuration").should("be.visible");
+  });
+
+  it("should switch to the Connections tab", () => {
+    cy.loginWithWidgetDemoPermissions();
+    cy.visit("/");
+    cy.findByRole("link", { name: SIDE_NAV_DEMO_LINK_TEXT }).click();
+    cy.get('button[role="tab"]').contains("Connections").click();
+    cy.contains("h3", "Configuration").should("not.exist");
   });
 });
--- apps/ucp-ui/src/Demo/PhoneContainer.test.tsx
+++ apps/ucp-ui/src/Demo/PhoneContainer.test.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
-import "@testing-library/jest-dom";
+import { render, screen, userEvent } from "../shared/test/testUtils";
 import PhoneContainer from "./PhoneContainer";
 
 describe("PhoneContainer", () => {
@@ -23,10 +22,10 @@ describe("PhoneContainer", () => {
     expect(resetButton).toBeInTheDocument();
   });
 
-  it("calls onReset when the reset button is clicked", () => {
+  it("calls onReset when the reset button is clicked", async () => {
     render(<PhoneContainer {...mockProps} />);
     const resetButton = screen.getByRole("button", { name: /reset/i });
-    fireEvent.click(resetButton);
+    await userEvent.click(resetButton);
     expect(mockProps.onReset).toHaveBeenCalledTimes(1);
   });
 });
