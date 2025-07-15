91f6bcd1116b8a8bcf8b29f6a38c1687dfee6bcf
Wes Risenmay <wes.risenmay@mx.com>
2025-07-15 09:23:57 -0600

    moved updateInstitution to the scream architecture

--- apps/institution-service/src/controllers/institutionController.test.ts
+++ apps/institution-service/src/controllers/institutionController.test.ts
@@ -2,25 +2,18 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { UUID } from "crypto";
 import { Request, Response } from "express";
-import { Model } from "sequelize";
 import { v4 as uuidv4 } from "uuid";
 
 import { Institution } from "../models/institution";
 import {
   cachedInstitutionFromSeed,
-  seedInstitutionId,
   seedInstitutionName,
   testInstitution,
 } from "../test/testData/institutions";
 import {
   createInstitution,
   getInstitutionCachedList,
-  updateInstitution,
 } from "./institutionController";
-
-const createNewInstitution = async () => {
-  return await Institution.create(testInstitution);
-};
 
 describe("institutionController", () => {
   describe("getInstitutionCachedList", () => {
@@ -121,103 +114,6 @@ describe("institutionController", () => {
       await createInstitution(req, res);
 
       expect(res.status).toHaveBeenCalledWith(201);
-    });
-  });
-
-  describe("updateInstitution", () => {
-    it("responds with 404 when institutionId is invalid", async () => {
-      const invalidInstitutionId = "invalidInstitutionId";
-      const req = {
-        params: { id: invalidInstitutionId },
-        body: { name: "newName" },
-      } as unknown as Request;
-
-      const res = {
-        json: jest.fn(),
-        status: jest.fn().mockReturnThis(),
-      } as unknown as Response;
-
-      await updateInstitution(req, res);
-      expect(res.status).toHaveBeenCalledWith(404);
-      expect(res.json).toHaveBeenCalledWith({
-        error: "Institution not found",
-      });
-    });
-
-    it("responds with 404 when institution is not found", async () => {
-      const nonExistentInstitutionId = "b2c01271-10e3-4f24-9236-44719e41fb40";
-      const req = {
-        params: { id: nonExistentInstitutionId },
-        body: { name: "newName" },
-      } as unknown as Request;
-
-      const res = {
-        json: jest.fn(),
-        status: jest.fn().mockReturnThis(),
-      } as unknown as Response;
-
-      await updateInstitution(req, res);
-      expect(res.status).toHaveBeenCalledWith(404);
-      expect(res.json).toHaveBeenCalledWith({
-        error: "Institution not found",
-      });
-    });
-
-    it("responds with 200 when institution exists and attributes are valid", async () => {
-      const institution = await createNewInstitution();
-      const existingInstitutionId = institution.id;
-
-      const updateBody = {
-        name: "newName",
-        keywords: ["newKeywords"],
-        logo: "newLogo",
-        url: "newUrl",
-        is_test_bank: true,
-        routing_numbers: ["123456789"],
-      };
-
-      const req = {
-        params: { id: existingInstitutionId },
-        body: updateBody,
-      } as unknown as Request;
-
-      const res = {
-        json: jest.fn(),
-        status: jest.fn().mockReturnThis(),
-      } as unknown as Response;
-
-      await updateInstitution(req, res);
-      expect(res.status).toHaveBeenCalledWith(200);
-      expect(res.json).toHaveBeenCalledWith({
-        message: "Institution updated successfully",
-        institution: expect.objectContaining({
-          ...updateBody,
-          id: existingInstitutionId,
-        }),
-      });
-    });
-
-    it("responds with 500 when there's an error updating the institution", async () => {
-      const req = {
-        params: { id: seedInstitutionId },
-        body: {},
-      } as unknown as Request;
-
-      const res = {
-        json: jest.fn(),
-        status: jest.fn().mockReturnThis(),
-      } as unknown as Response;
-
-      jest.spyOn(Institution, "findByPk").mockResolvedValue({
-        update: jest.fn().mockRejectedValue(new Error("Server broke")),
-      } as unknown as Model);
-
-      await updateInstitution(req, res);
-
-      expect(res.status).toHaveBeenCalledWith(500);
-      expect(res.json).toHaveBeenCalledWith({
-        error: "An error occurred while updating the institution",
-      });
     });
   });
 });
--- apps/institution-service/src/controllers/institutionController.ts
+++ apps/institution-service/src/controllers/institutionController.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
 import { ValidationError } from "sequelize";
-import { validate } from "uuid";
 import { Aggregator } from "../models/aggregator";
 import { Institution } from "../models/institution";
 import { transformInstitutionToCachedInstitution } from "../services/institutionService";
@@ -64,42 +63,6 @@ export const createInstitution = async (req: Request, res: Response) => {
     } else {
       res.status(400).json({ error: error });
     }
-  }
-};
-
-interface updateInstitutionParams {
-  name: string;
-  keywords: string[];
-  logo: string;
-  url: string;
-  is_test_bank: boolean;
-  routing_numbers: string[];
-}
-
-export const updateInstitution = async (req: Request, res: Response) => {
-  try {
-    const institutionId = req.params.id;
-    const updateData = req.body as updateInstitutionParams;
-
-    if (!validate(institutionId)) {
-      return res.status(404).json({ error: "Institution not found" });
-    }
-
-    const institution = await Institution.findByPk(institutionId);
-
-    if (!institution) {
-      return res.status(404).json({ error: "Institution not found" });
-    }
-
-    await institution.update(updateData);
-
-    return res
-      .status(200)
-      .json({ message: "Institution updated successfully", institution });
-  } catch (error) {
-    return res
-      .status(500)
-      .json({ error: "An error occurred while updating the institution" });
   }
 };
 
