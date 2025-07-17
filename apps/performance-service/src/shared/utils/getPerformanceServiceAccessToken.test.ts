bfd90a72b289b0bde8e22aabafe50594ce01c6ec
Wes Risenmay <wes.risenmay@mx.com>
2025-07-17 10:37:18 -0600

    fix unit test

--- apps/performance-service/src/shared/utils/getPerformanceServiceAccessToken.test.ts
+++ apps/performance-service/src/shared/utils/getPerformanceServiceAccessToken.test.ts
@@ -1,91 +1,91 @@
-// import { http, HttpResponse } from "msw";
-// import { server } from "../tests/";
-// import {
-//   clearTokenCache,
-//   getPerformanceServiceAccessToken,
-// } from "./getPerformanceServiceAccessToken";
-// import { AUTH0_AUTH_TOKEN_URL } from "../test/handlers";
-// import { tokenResponse } from "../test/testData/auth0";
+import { http, HttpResponse } from "msw";
+import { server } from "../../shared/tests/testServer";
+import {
+  clearTokenCache,
+  getPerformanceServiceAccessToken,
+} from "./getPerformanceServiceAccessToken";
+import { AUTH0_AUTH_TOKEN_URL } from "../../shared/tests/handlers";
+import { tokenResponse } from "../../shared/tests/testData/auth0";
 
-// describe("getPerformanceServiceAccessToken", () => {
-//   beforeEach(() => {
-//     clearTokenCache();
-//   });
+describe("getPerformanceServiceAccessToken", () => {
+  beforeEach(() => {
+    clearTokenCache();
+  });
 
-//   afterEach(() => {
-//     clearTokenCache();
-//   });
+  afterEach(() => {
+    clearTokenCache();
+  });
 
-//   it("fetches a new access token if the cache is empty and returns a cached access token if it is still valid", async () => {
-//     const testToken = "testTokenForThisTest";
+  it("fetches a new access token if the cache is empty and returns a cached access token if it is still valid", async () => {
+    const testToken = "testTokenForThisTest";
 
-//     server.use(
-//       http.post(AUTH0_AUTH_TOKEN_URL, () =>
-//         HttpResponse.json({
-//           ...tokenResponse,
-//           access_token: testToken,
-//         }),
-//       ),
-//     );
+    server.use(
+      http.post(AUTH0_AUTH_TOKEN_URL, () =>
+        HttpResponse.json({
+          ...tokenResponse,
+          access_token: testToken,
+        }),
+      ),
+    );
 
-//     const token = await getPerformanceServiceAccessToken();
+    const token = await getPerformanceServiceAccessToken();
 
-//     expect(token).toBe(testToken);
+    expect(token).toBe(testToken);
 
-//     server.use(
-//       http.post(AUTH0_AUTH_TOKEN_URL, () => HttpResponse.json(tokenResponse)),
-//     );
+    server.use(
+      http.post(AUTH0_AUTH_TOKEN_URL, () => HttpResponse.json(tokenResponse)),
+    );
 
-//     const token2 = await getPerformanceServiceAccessToken();
+    const token2 = await getPerformanceServiceAccessToken();
 
-//     expect(token2).toBe(testToken);
-//   });
+    expect(token2).toBe(testToken);
+  });
 
-//   it("fetches a new access token if the cached token has expired", async () => {
-//     const expiredToken = "expiredToken";
+  it("fetches a new access token if the cached token has expired", async () => {
+    const expiredToken = "expiredToken";
 
-//     server.use(
-//       http.post(AUTH0_AUTH_TOKEN_URL, () =>
-//         HttpResponse.json({
-//           ...tokenResponse,
-//           access_token: expiredToken,
-//           expires_in: 0,
-//         }),
-//       ),
-//     );
+    server.use(
+      http.post(AUTH0_AUTH_TOKEN_URL, () =>
+        HttpResponse.json({
+          ...tokenResponse,
+          access_token: expiredToken,
+          expires_in: 0,
+        }),
+      ),
+    );
 
-//     const token = await getPerformanceServiceAccessToken();
+    const token = await getPerformanceServiceAccessToken();
 
-//     expect(token).toBe(expiredToken);
+    expect(token).toBe(expiredToken);
 
-//     const newToken = "newTokenAfterExpiry";
+    const newToken = "newTokenAfterExpiry";
 
-//     server.use(
-//       http.post(AUTH0_AUTH_TOKEN_URL, () =>
-//         HttpResponse.json({
-//           ...tokenResponse,
-//           access_token: newToken,
-//         }),
-//       ),
-//     );
+    server.use(
+      http.post(AUTH0_AUTH_TOKEN_URL, () =>
+        HttpResponse.json({
+          ...tokenResponse,
+          access_token: newToken,
+        }),
+      ),
+    );
 
-//     const token2 = await getPerformanceServiceAccessToken();
+    const token2 = await getPerformanceServiceAccessToken();
 
-//     expect(token2).toBe(newToken);
-//   });
+    expect(token2).toBe(newToken);
+  });
 
-//   it("throws an error if the token request fails", async () => {
-//     const statusText = "testStatusText";
+  it("throws an error if the token request fails", async () => {
+    const statusText = "testStatusText";
 
-//     server.use(
-//       http.post(
-//         AUTH0_AUTH_TOKEN_URL,
-//         () => new HttpResponse(null, { status: 400, statusText }),
-//       ),
-//     );
+    server.use(
+      http.post(
+        AUTH0_AUTH_TOKEN_URL,
+        () => new HttpResponse(null, { status: 400, statusText }),
+      ),
+    );
 
-//     await expect(getPerformanceServiceAccessToken()).rejects.toThrow(
-//       `Auth0 token request failed: ${statusText}`,
-//     );
-//   });
-// });
+    await expect(getPerformanceServiceAccessToken()).rejects.toThrow(
+      `Auth0 token request failed: ${statusText}`,
+    );
+  });
+});
