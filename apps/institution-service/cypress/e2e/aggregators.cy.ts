77bed8cce500f5d0dfb515061060c41571d612d6
Wes Risenmay <wes.risenmay@mx.com>
2025-07-21 14:14:40 -0600

    added a test for the performance auth aggregators endpoint

--- apps/institution-service/cypress/e2e/aggregators.cy.ts
+++ apps/institution-service/cypress/e2e/aggregators.cy.ts
@@ -1,29 +1,13 @@
-import { Aggregator } from "models/aggregator";
 import { runTokenInvalidCheck } from "../support/utils";
-import { getAggregators } from "../shared/utils/aggregator";
+import {
+  expectLooksLikeAggregators,
+  getAggregators,
+} from "../shared/utils/aggregator";
 
 describe("aggregators", () => {
   describe("/aggregators GET", () => {
     it("returns a list of aggregators for a valid token", () => {
-      getAggregators().then(
-        (response: Cypress.Response<{ aggregators: Aggregator[] }>) => {
-          expect(response.status).to.eq(200);
-
-          expect(response.body.aggregators.length).to.be.greaterThan(0);
-          response.body.aggregators.forEach((aggregator) => {
-            [
-              "id",
-              "name",
-              "displayName",
-              "logo",
-              "createdAt",
-              "updatedAt",
-            ].forEach((attribute) => {
-              expect(aggregator).to.haveOwnProperty(attribute);
-            });
-          });
-        },
-      );
+      getAggregators().then(expectLooksLikeAggregators);
     });
 
     runTokenInvalidCheck({
--- apps/institution-service/cypress/shared/utils/aggregator.ts
+++ apps/institution-service/cypress/shared/utils/aggregator.ts
@@ -1,3 +1,4 @@
+import { Aggregator } from "models/aggregator";
 import { USER_ACCESS_TOKEN_ENV } from "../constants/accessTokens";
 import { createAuthorizationHeader } from "./authorization";
 
@@ -9,5 +10,20 @@ export const getAggregators = () => {
     headers: {
       Authorization: createAuthorizationHeader(USER_ACCESS_TOKEN_ENV),
     },
+  });
+};
+
+export const expectLooksLikeAggregators = (
+  response: Cypress.Response<{ aggregators: Aggregator[] }>,
+) => {
+  expect(response.status).to.eq(200);
+
+  expect(response.body.aggregators.length).to.be.greaterThan(0);
+  response.body.aggregators.forEach((aggregator) => {
+    ["id", "name", "displayName", "logo", "createdAt", "updatedAt"].forEach(
+      (attribute) => {
+        expect(aggregator).to.haveOwnProperty(attribute);
+      },
+    );
   });
 };
