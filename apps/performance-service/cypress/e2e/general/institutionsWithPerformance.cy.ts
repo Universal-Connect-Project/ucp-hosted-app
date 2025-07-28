5dba359b5f7087c505359dfa7cafe7952de758c0
Wes Risenmay <wes.risenmay@mx.com>
2025-07-28 15:12:58 -0600

    log to cypress instead

--- apps/performance-service/cypress/e2e/general/institutionsWithPerformance.cy.ts
+++ apps/performance-service/cypress/e2e/general/institutionsWithPerformance.cy.ts
@@ -57,7 +57,7 @@ describe("institutions with performance metrics", () => {
           expect(response.body.institutions).to.be.an("array");
           expect(response.body.institutions).to.have.length.greaterThan(0);
 
-          console.log(
+          cy.log(
             "Institutions with performance metrics:",
             response.body.institutions,
           );
