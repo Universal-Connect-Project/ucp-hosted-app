0851a34770545bca9158cb50f2b040fa99c03dc3
Ashley Wright <ashley.wright@mx.com>
2025-07-28 09:43:25 -0600

    fix issue with failing test

--- apps/ucp-ui/src/WidgetDemo/ConnectWidget.tsx
+++ apps/ucp-ui/src/WidgetDemo/ConnectWidget.tsx
@@ -23,7 +23,7 @@ const ConnectWidget = ({
 }) => {
   const userId = React.useMemo(() => {
     const array = new Uint8Array(16);
-    window.crypto.getRandomValues(array);
+    crypto.getRandomValues(array);
     return Array.from(array, (byte) => byte.toString(16).padStart(2, "0")).join(
       "",
     );
