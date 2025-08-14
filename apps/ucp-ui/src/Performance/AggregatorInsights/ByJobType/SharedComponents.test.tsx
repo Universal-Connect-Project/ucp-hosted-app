17a18bade20304ab97f63029fdac0b8ec008917b
Wes Risenmay <wes.risenmay@mx.com>
2025-08-14 08:35:44 -0600

    remove old test

--- apps/ucp-ui/src/Performance/AggregatorInsights/ByJobType/SharedComponents.test.tsx
+++ apps/ucp-ui/src/Performance/AggregatorInsights/ByJobType/SharedComponents.test.tsx
@@ -1,11 +1,7 @@
 import React from "react";
-import { Table, TableBody, TableCell, TableRow } from "@mui/material";
-import {
-  expectSkeletonLoader,
-  render,
-  screen,
-} from "../../../shared/test/testUtils";
-import { NoDataCell, TableRowWithPaddingCells } from "./SharedComponents";
+import { Table, TableBody, TableCell } from "@mui/material";
+import { render } from "../../../shared/test/testUtils";
+import { TableRowWithPaddingCells } from "./SharedComponents";
 
 describe("Shared components", () => {
   describe("<TableRowWithPaddingCells />", () => {
@@ -21,63 +17,6 @@ describe("Shared components", () => {
       );
 
       expect(container.querySelectorAll("td")).toHaveLength(3);
-    });
-  });
-
-  describe("<NoDataCell />", () => {
-    it("renders children if hasData", () => {
-      const children = "testChildren";
-
-      render(
-        <Table>
-          <TableBody>
-            <TableRow>
-              <NoDataCell hasData isLoading={false}>
-                {children}
-              </NoDataCell>
-            </TableRow>
-          </TableBody>
-        </Table>,
-      );
-
-      expect(screen.getByText(children)).toBeInTheDocument();
-    });
-
-    it("renders 'No data' if !hasData", () => {
-      const children = "testChildren";
-
-      render(
-        <Table>
-          <TableBody>
-            <TableRow>
-              <NoDataCell hasData={false} isLoading={false}>
-                {children}
-              </NoDataCell>
-            </TableRow>
-          </TableBody>
-        </Table>,
-      );
-
-      expect(screen.queryByText(children)).not.toBeInTheDocument();
-      expect(screen.getByText("No data")).toBeInTheDocument();
-    });
-
-    it("renders a loading state if isLoading", async () => {
-      const children = "testChildren";
-
-      render(
-        <Table>
-          <TableBody>
-            <TableRow>
-              <NoDataCell hasData isLoading={true}>
-                {children}
-              </NoDataCell>
-            </TableRow>
-          </TableBody>
-        </Table>,
-      );
-
-      await expectSkeletonLoader();
     });
   });
 });
