20e7704ebafead53a9cb1a77afba717b93bef717
Wes Risenmay <wes.risenmay@mx.com>
2025-10-07 15:25:32 -0600

    adding a logger to the review app proxy server

--- proxyServer.js
+++ proxyServer.js
@@ -5,9 +5,9 @@ const path = require('path');
 const app = express();
 
 // Forward requests to internal services
-app.use('/institution-service', createProxyMiddleware({ target: 'http://localhost:8088', changeOrigin: true }));
-app.use('/authentication-service', createProxyMiddleware({ target: 'http://localhost:8089', changeOrigin: true }));
-app.use('/performance-service', createProxyMiddleware({ target: 'http://localhost:8090', changeOrigin: true }));
+app.use('/institution-service', createProxyMiddleware({ target: 'http://localhost:8088', changeOrigin: true, logger: console }));
+app.use('/authentication-service', createProxyMiddleware({ target: 'http://localhost:8089', changeOrigin: true, logger: console }));
+app.use('/performance-service', createProxyMiddleware({ target: 'http://localhost:8090', changeOrigin: true, logger: console }));
 
 app.use('/', express.static('apps/ucp-ui/dist'));
 
