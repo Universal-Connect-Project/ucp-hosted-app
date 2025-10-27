70f1cd6c74b7840e145c255b8bc3da48f1928474
Wes Risenmay <wes.risenmay@mx.com>
2025-10-27 13:52:34 -0600

    finished one test for matchInstitutions

--- apps/institution-service/src/aggregatorInstitutions/match/matchInstitutions.test.ts
+++ apps/institution-service/src/aggregatorInstitutions/match/matchInstitutions.test.ts
@@ -1,21 +1,71 @@
 import { AggregatorInstitution } from "../../models/aggregatorInstitution";
 import { AggregatorIntegration } from "../../models/aggregatorIntegration";
 import { Institution } from "../../models/institution";
+import { getAggregatorByName } from "../../shared/aggregators/getAggregatorByName";
+import { createTestInstitution } from "../../test/createTestInstitution";
+import { matchInstitutions } from "./matchInstitutions";
 
 describe("matchInstitutions", () => {
   beforeEach(async () => {
-    await AggregatorIntegration.destroy({
-      force: true,
-      truncate: true,
-    });
-    await AggregatorInstitution.destroy({
-      force: true,
-      truncate: true,
-    });
-    await Institution.truncate({ force: true, cascade: true });
+    await Institution.truncate({ cascade: true });
+    await AggregatorInstitution.truncate({ force: true });
+    await AggregatorIntegration.truncate({ force: true });
   });
 
-  it("creates aggregator integrations for institutions with similar names and urls", () => {});
+  afterEach(async () => {
+    await Institution.truncate({ cascade: true });
+    await AggregatorInstitution.truncate({ force: true });
+    await AggregatorIntegration.truncate({ force: true });
+  });
+
+  it("creates aggregator integrations for institutions with similar names and urls", async () => {
+    const { institution: institution1 } = await createTestInstitution({
+      institution: {
+        name: "Star Wars Episode 1: The Phantom Bank",
+        url: "https://www.starwarsepisode1thephantonbank.com",
+      },
+    });
+
+    const { institution: institution2 } = await createTestInstitution({
+      institution: {
+        name: "Harry Potter and the Bank of Azkaban",
+        url: "https://www.harrypotterandthebankofazkaban.com",
+      },
+    });
+
+    const mxAggregatorId = (await getAggregatorByName("mx")).id;
+    const testBankAggregatorInstitution = await AggregatorInstitution.create({
+      id: "test_bank_aggregator_institution_id",
+      aggregatorId: mxAggregatorId,
+      name: "Star Wars Episode 1: The Phantom Bankz",
+      url: "https://www.starwarsepisode1thephantonbankz.com",
+    });
+
+    const institution2AggregatorInstitution =
+      await AggregatorInstitution.create({
+        id: "wells_fargo_aggregator_institution_id",
+        aggregatorId: mxAggregatorId,
+        name: "Harry Potter and the Bank of Azkabanz",
+        url: "https://www.harrypotterandthebankofazkabanz.com",
+      });
+
+    await matchInstitutions(mxAggregatorId);
+
+    const institution1AggregatorIntegrations =
+      await institution1.getAggregatorIntegrations();
+
+    expect(institution1AggregatorIntegrations).toHaveLength(1);
+    expect(
+      institution1AggregatorIntegrations[0].aggregator_institution_id,
+    ).toBe(testBankAggregatorInstitution.id);
+
+    const institution2AggregatorIntegrations =
+      await institution2.getAggregatorIntegrations();
+    expect(institution2AggregatorIntegrations).toHaveLength(1);
+    expect(
+      institution2AggregatorIntegrations[0].aggregator_institution_id,
+    ).toBe(institution2AggregatorInstitution.id);
+  });
 
   it("doesn't create more than 1 aggregator integration for the same institution even if it would normally match", () => {});
 
